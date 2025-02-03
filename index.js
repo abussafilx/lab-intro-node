b8ed61486cedc050e97c3da4e3e254a334d0c938
Victor Abussafi <victorabussafi@gmail.com>
2025-02-03 15:31:10 +0000

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items = []) {
+    this.items = items;
+    this.length = items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length
+    this.items.sort((a, b) => a - b);
+    return this.items;
 
-  get(pos) {}
+  }
 
-  max() {}
+  get(pos) {
+    if (pos > this.length) {
+      throw new Error("OutOfBounds");
+    } else { return this.items[pos] }
+  }
 
-  min() {}
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[this.length - 1]
+    }
+  }
 
-  sum() {}
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.items[0]
+    }
 
-  avg() {}
+  }
+
+  sum() {
+    if (this.length === 0) {
+      return 0
+    } else {
+      return this.items.reduce(
+        (acc, currentValue) => acc + currentValue,
+        0
+      )
+
+    }
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList")
+    } else {
+      return this.items.reduce(
+        (acc, currentValue) => acc + currentValue,
+        0
+      )/this.length
+
+    }
+  }
 }
 
 module.exports = SortedList;
